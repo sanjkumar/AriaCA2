a4bb835e8321734398b2d61d0dae90115c80a8ce
sanjkumar <sanj.judh@gmail.com>
2014-03-26 19:39:28 +0000

    jasmin

---
+++ test/js/addvalue.js
@@ -0,0 +1,13 @@
+/**
+ * Created by sanjeev on 26/03/2014.
+ */
+
+function add(){
+    var a,b,c;
+    a=Number(document.getElementById("first").value);
+    b=Number(document.getElementById("second").value);
+    c= a + b;
+    document.getElementById("answer").value= c;
+}
+
+
